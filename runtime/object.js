81d8fc706756bbfaebbe2769db6fdfc97dca59b5
Chris Dickinson <christopher.s.dickinson@gmail.com>
2016-03-12 16:13:41 -0800

    add standard

--- runtime/object.js
+++ runtime/object.js
@@ -2,14 +2,12 @@ module.exports = makeObject
 
 var SharedFunctionInfo = require('../lib/values/shared-function-info.js')
 var FunctionValue = require('../lib/values/function.js')
-var hidden = require('../lib/values/hidden-class.js')
-var ObjectValue = require('../lib/values/object.js')
 
-function makeObject(cfg, globals, quickFn) {
+function makeObject (cfg, globals, quickFn) {
   var builtins = cfg._builtins
   var functionProto = builtins.getprop('[[FunctionProto]]').value()
   var objectProto = builtins.getprop('[[ObjectProto]]').value()
-  var toStringAST = {"type":"FunctionDeclaration","id":{"type":"Identifier","name":"toString"},"params":[],"defaults":[],"body":{"type":"BlockStatement","body":[{"type":"ReturnStatement","argument":{"type":"BinaryExpression","operator":"+","left":{"type":"BinaryExpression","operator":"+","left":{"type":"Literal","value":"[object ","raw":"\'[object \'"},"right":{"type":"MemberExpression","computed":false,"object":{"type":"MemberExpression","computed":false,"object":{"type":"ThisExpression"},"property":{"type":"Identifier","name":"constructor"}},"property":{"type":"Identifier","name":"name"}}},"right":{"type":"Literal","value":"]","raw":"\']\'"}}}]},"rest":null,"generator":false,"expression":false}
+  var toStringAST = {'type': 'FunctionDeclaration', 'id': {'type': 'Identifier', 'name': 'toString'}, 'params': [], 'defaults': [], 'body': {'type': 'BlockStatement', 'body': [{'type': 'ReturnStatement', 'argument': {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'BinaryExpression', 'operator': '+', 'left': {'type': 'Literal', 'value': '[object ', 'raw': '\'[object \''}, 'right': {'type': 'MemberExpression', 'computed': false, 'object': {'type': 'MemberExpression', 'computed': false, 'object': {'type': 'ThisExpression'}, 'property': {'type': 'Identifier', 'name': 'constructor'}}, 'property': {'type': 'Identifier', 'name': 'name'}}}, 'right': {'type': 'Literal', 'value': ']', 'raw': '\']\''}}}]}, 'rest': null, 'generator': false, 'expression': false}
 
   var toStringSFI = new SharedFunctionInfo(toStringAST)
   var objectCons = quickFn('Object', ObjectImpl, globals)
@@ -28,14 +26,14 @@ function makeObject(cfg, globals, quickFn) {
   quickFn('create', ObjectCreateImpl, objectCons)
 }
 
-function ObjectImpl(cfg, thisValue, args, isNew) {
+function ObjectImpl (cfg, thisValue, args, isNew) {
   // TODO: make this more accurate!
   cfg._valueStack.push(
     cfg.makeObject()
   )
 }
 
-function ObjectCreateImpl(cfg, thisValue, args, isNew) {
+function ObjectCreateImpl (cfg, thisValue, args, isNew) {
   cfg._valueStack.push(
     cfg.makeObject(null, args[0] || cfg.makeUnknown())
   )
